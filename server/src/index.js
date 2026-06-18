727dc5e538c87a0a596c482c07915fb4c4939051
Esther Obisesan <est0295@gmail.com>
2026-06-18 08:34:14 +0100

    fix: correct frontend url

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,14 @@ app.use(
   }),
 );
 
+/* app.use(cors({
+  origin: [
+    'https://bemsfarms.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
+ */
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // CRITICAL: parse cookies
